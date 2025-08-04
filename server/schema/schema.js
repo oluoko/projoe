b182aa510f95475cb395de8b3f254221719b170e
oluoko <brianotieno586@gmail.com>
2025-08-04 08:53:56 +0300

    Projoe: Setting up Mongoose Models

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,4 +1,7 @@
 const { projects, clients } = require("../sampleData.js");
+// Mongoose models
+const Project = require("../models/Project.js");
+const Client = require("../models/Client.js");
 
 const {
   GraphQLObjectType,
@@ -30,7 +33,7 @@ const ProjectType = new GraphQLObjectType({
     client: {
       type: ClientType,
       resolve(parent, args) {
-        return clients.find((client) => client.id === parent.clientId);
+        return Client.findById(parent.clientId);
       },
     },
   }),
@@ -42,27 +45,27 @@ const RootQuery = new GraphQLObjectType({
     clients: {
       type: new GraphQLList(ClientType),
       resolve(parent, args) {
-        return clients;
+        return Client.find();
       },
     },
     client: {
       type: ClientType,
       args: { id: { type: GraphQLID } },
       resolve(parent, args) {
-        return clients.find((client) => client.id === args.id);
+        return Client.findById(args.id);
       },
     },
     projects: {
       type: new GraphQLList(ProjectType),
       resolve(parent, args) {
-        return projects;
+        return Project.find();
       },
     },
     project: {
       type: ProjectType,
       args: { id: { type: GraphQLID } },
       resolve(parent, args) {
-        return projects.find((project) => project.id === args.id);
+        return Project.findById(args.id);
       },
     },
   },
