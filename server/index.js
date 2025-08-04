14228e4555e186c4051fc148cf63f9129872ec21
oluoko <brianotieno586@gmail.com>
2025-08-04 08:37:45 +0300

    Projoe: Database setup

--- server/index.js
+++ server/index.js
@@ -2,9 +2,14 @@ const express = require("express");
 require("dotenv").config();
 const { graphqlHTTP } = require("express-graphql");
 const schema = require("./schema/schema");
+const colors = require("colors");
+const connectDB = require("./config/db");
 const port = process.env.PORT || 5000;
 
 const app = express();
+
+// Connect to database
+connectDB();
 
 app.use(
   "/graphql",
